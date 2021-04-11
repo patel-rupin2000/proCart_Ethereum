1bf792eb598f51a0c5b20cc43027e6082173fe5f
Shresth Raj <58912373+Shresth6raj@users.noreply.github.com>
2021-04-11 16:21:58 +0530

    js file added

---
+++ views/a.js
@@ -0,0 +1,49 @@
+function product_1(){
+    var x= document.getElementById('details_1').innerHTML;
+    document.getElementById('a_a').innerHTML=x;
+    var x_1= document.getElementById('pro_title_1').innerHTML;
+    document.getElementById('b_b').innerHTML=x_1;
+    var x_2= document.getElementById('image_1').innerHTML;
+    document.getElementById('c_c').innerHTML=x_2;
+    console.log(x_2)
+    
+    // let new_html='';
+
+    // new_html += x;
+    // new_html += '<p>hello</p>';
+
+    // $('#a_a').find('tbody').append(new_html);
+
+}
+function product_2(){
+    var x= document.getElementById('details_2').innerHTML;
+    document.getElementById('a_a').innerHTML=x;
+    var x_1= document.getElementById('pro_title_2').innerHTML;
+    document.getElementById('b_b').innerHTML=x_1;
+    var x_2= document.getElementById('image_2').innerHTML;
+    document.getElementById('c_c').innerHTML=x_2;
+    console.log(x_2)
+    // let new_html='';
+
+    // new_html += x;
+    // new_html += '<p>hello</p>';
+
+    // $('#a_a').find('tbody').append(new_html);
+
+}
+function product_3(){
+    var x= document.getElementById('details_3').innerHTML;
+    document.getElementById('a_a').innerHTML=x;
+    var x_1= document.getElementById('pro_title_3').innerHTML;
+    document.getElementById('b_b').innerHTML=x_1;
+    var x_2= document.getElementById('image_3').innerHTML;
+    document.getElementById('c_c').innerHTML=x_2;
+    console.log(x_2)
+    // let new_html='';
+
+    // new_html += x;
+    // new_html += '<p>hello</p>';
+
+    // $('#a_a').find('tbody').append(new_html);
+
+}
